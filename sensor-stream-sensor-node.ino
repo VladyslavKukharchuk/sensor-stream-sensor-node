1709b00af00b535ac8ce5e0eb310240ede06a794
Vladyslav Kukharchuk <vladyslav.kukharchuk@makeheadway.com>
2025-11-11 22:34:54 +0200

    Add WiFi connect and secrets store

--- sensor-stream-sensor-node.ino
+++ sensor-stream-sensor-node.ino
@@ -1,14 +1,31 @@
 #include "DHT.h"
+#include <WiFi.h>
+#include "secrets.h"
 
 #define DHTPIN 4
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
+void connectToWiFi() {
+  Serial.print("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Connected! IP: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("ESP32 start!");
   dht.begin();
+  connectToWiFi();
 }
 
 void loop() {
